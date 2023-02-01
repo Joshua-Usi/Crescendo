fcae9f519367751862d87eb8e75d81bf6ed125a4
Joshua usi <joshua.usi2017@gmail.com>
2023-02-02 03:43:51 +1100

    Added dependencies updating

--- DownloadDependencies.bat
+++ DownloadDependencies.bat
@@ -1,1 +1,12 @@
+@echo off
+
+echo Downloading dependencies
 git submodule add https://github.com/Joshua-Usi/CrescendoThirdParty ./Crescendo/ThirdParty
+if errorlevel 1 goto Update
+pause
+
+:Update
+echo Dependencies already exist
+echo Updating dependencies
+git submodule update --recursive --remote
+pause
