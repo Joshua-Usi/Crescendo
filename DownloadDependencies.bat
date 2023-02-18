1475f84fbae7db9ee42aeafac1cfff3faf0b2090
Joshua usi <joshua.usi2017@gmail.com>
2023-02-18 17:12:33 +1100

    Made Project compile on first checkout
    Makes it easier for everyone

--- DownloadDependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Downloading dependencies
-git submodule add https://github.com/Joshua-Usi/CrescendoThirdParty ./Crescendo/ThirdParty
-git submodule add https://github.com/Joshua-Usi/Mochi ./Tests/Mochi
-
-echo Updating dependencies
-git submodule update --recursive --remote
-pause
--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,2 +1,12 @@
+@echo off
+
+echo Downloading dependencies
+git clone https://github.com/Joshua-Usi/CrescendoThirdParty ./Crescendo/ThirdParty
+
+git clone https://github.com/Joshua-Usi/Mochi ./Tests/Mochi
+
+echo Updating dependencies
+git submodule update --recursive --remote
+
 vendor\bins\premake\premake5.exe --file=GenerateProjectFiles.lua vs2022
 pause
