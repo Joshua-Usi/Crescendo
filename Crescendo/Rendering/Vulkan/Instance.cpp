89ff1969aeed700d5a56667fb7e2dcbfdfa722ee
Joshua Usi <joshua.usi2017@gmail.com>
2024-01-22 12:55:45 +1100

    Automatically select suitable devices

--- Crescendo/Rendering/Vulkan/Instance.cpp
+++ Crescendo/Rendering/Vulkan/Instance.cpp
@@ -43,7 +43,24 @@ CS_NAMESPACE_BEGIN::Vulkan
 		// Sometimes it can choose integrated, then we need to determine which one we want
 		auto physicalDeviceResult = vkb::PhysicalDeviceSelector(instance).set_minimum_version(CS_VK_MAJOR, CS_VK_MINOR).set_surface(this->surface).set_required_features(deviceFeatures).select_devices();
 		if (!physicalDeviceResult) cs_std::console::fatal("Failed to select Vulkan physical device!", physicalDeviceResult.error().message());
-		this->vkbPhysicalDevice = physicalDeviceResult.value()[1];
+
+
+		bool foundDiscrete = false;
+		for (const auto& device : physicalDeviceResult.value())
+		{
+			if (device.properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+			{
+				this->vkbPhysicalDevice = device;
+				foundDiscrete = true;
+				break;
+			}
+		}
+		// If we didn't find a discrete GPU, select the first device by default
+		if (!foundDiscrete)
+		{
+			this->vkbPhysicalDevice = physicalDeviceResult.value()[0];
+			cs_std::console::log("Failed to find discrete GPU, falling back to first device");
+		}
 	}
 	Instance::~Instance()
 	{
